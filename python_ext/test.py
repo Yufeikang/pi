3ca591db2c73e37d2f179a84e72ac5528819d092
kang <kyf0722@gmail.com>
2017-01-07 22:55:25 +0800

    fix

--- python_ext/test.py
+++ python_ext/test.py
@@ -1,39 +1,40 @@
-import  SensorLib as s
-import  time
-ret =s.Init()
+import SensorLib as s
+import time
+
+ret = s.Init()
 if ret == 0:
     print("Init Ok!")
 else:
     print("Init ERR!")
-humidity,temp,status= s.GetAM2301Data()
+humidity, temp, status = s.GetAM2301Data()
 
 print("humidity:%f" % humidity)
 print("temperature:%f" % temp)
 print("status:%f" % status)
-s.GPIOSetMode(7,"OUTPUT".encode("ascii"))
-s.GPIOSetPullUpDn(7,"PUD_OFF".encode("ascii"))
-s.GPIODWrite(7,0)
-lcdOn=True
+s.GPIOSetMode(7, "OUTPUT".encode("ascii"))
+s.GPIOSetPullUpDn(7, "PUD_OFF".encode("ascii"))
+s.GPIODWrite(7, 0)
+lcdOn = True
 while True:
-    humidity,temp,status= s.GetAM2301Data()
+    humidity, temp, status = s.GetAM2301Data()
     if status == 1:
-        s.LcdSendCmd(1);
-        s.LcdSendCmd(0x80);
+        s.LcdSendCmd(1)
+        s.LcdSendCmd(0x80)
         data = "湿度:%.2f hr" % humidity
-        s.LcdShow(data.encode("gbk"));
+        s.LcdShow(data.encode("gbk"))
         data = "温度：%.2f C" % temp
         s.LcdSendCmd(0x90)
         s.LcdShow(data.encode("gbk"))
         s.LcdSendCmd(0x88)
-        time_str=time.strftime("%A %H:%M")
+        time_str = time.strftime("%A %H:%M")
         s.LcdShow(time_str.encode("gbk"))
     time.sleep(2)
     hour = time.localtime().tm_hour
-    if hour>=9 and hour<23:# and lcdOn==False:
-        s.GPIODWrite(7,0)
-        lcdOn=True
+    if 9 <= hour < 22:  # and lcdOn==False:
+        s.GPIODWrite(7, 0)
+        lcdOn = True
         print("lcd on")
-    elif hour>=0 and hour<9:# and lcdOn==True:
-        s.GPIODWrite(7,1)
-        lcdOn=False
+    elif 0 <= hour < 9:  # and lcdOn==True:
+        s.GPIODWrite(7, 1)
+        lcdOn = False
         print("lcd off")
